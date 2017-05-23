2f7def7e2a1481c3eda98e7ac8742837c2892630
mihaioltean <moltean@cs.ubbcluj.ro>
2017-05-23 22:21:25 +0300

    added more fruits

--- src/fruits_definition.h
+++ src/fruits_definition.h
@@ -25,7 +25,7 @@ Rect r_box(745, 235, 775, 775);
 #define color_distance 1
 */
 
-//plum1_2017 05 10 17 17 53; OK
+//plum1_2017 05 10 17 17 53 ; OK
 /*
 string input_file_name = "c:\\Mihai\\Dropbox\\fruits\\plum1_2017 05 10 17 17 53";
 Rect r_box(870, 230, 500, 500);
@@ -33,7 +33,7 @@ Rect r_box(870, 230, 500, 500);
 #define color_distance 7
 */
 
-//plum_rotated_1_2017 05 10 17 19 49; OK
+//plum_rotated_1_2017 05 10 17 19 49 ; OK
 /*
 string input_file_name = "c:\\Mihai\\Dropbox\\fruits\\plum_rotated_1_2017 05 10 17 19 49";
 Rect r_box(890, 170, 490, 490);
@@ -41,7 +41,7 @@ Rect r_box(890, 170, 490, 490);
 #define color_distance 7
 */
 
-//apple1_2017 02 25 13 33 44; OK
+//apple1_2017 02 25 13 33 44 ; OK
 /*
 string input_file_name = "c:\\Mihai\\Dropbox\\fruits\\apple1_2017 02 25 13 33 44";
 Rect r_box(850, 330, 680, 680);
@@ -49,7 +49,7 @@ Rect r_box(850, 330, 680, 680);
 #define color_distance 5
 */
 
-//apple2_2017 02 25 13 34 34; OK
+//apple2_2017 02 25 13 34 34 ; OK
 /*
 string input_file_name = "c:\\Mihai\\Dropbox\\fruits\\apple2_2017 02 25 13 34 34";
 Rect r_box(825, 330, 730, 730);
@@ -73,10 +73,65 @@ Rect r_box(950, 260, 350, 350);
 #define color_distance 9
 */
 
-//braeburn_2017 03 05 08 34 49
+//braeburn_2017 03 05 08 34 49 ; OK
 /*
 string input_file_name = "c:\\Mihai\\Dropbox\\fruits\\braeburn_2017 03 05 08 34 49";
 Rect r_box(980, 400, 540, 540);
 #define motor_shaft_height 18
 #define color_distance 9
 */
+
+
+//braeburn_rotated_2017 03 05 08 35 37 ; OK
+/*
+string input_file_name = "c:\\Mihai\\Dropbox\\fruits\\braeburn_rotated_2017 03 05 08 35 37";
+Rect r_box(985, 300, 520, 520);
+#define motor_shaft_height 5
+#define color_distance 9
+*/
+
+//golden_1_2017 02 28 10 16 25
+/*
+string input_file_name = "c:\\Mihai\\Dropbox\\fruits\\golden_1_2017 02 28 10 16 25";
+Rect r_box(770, 280, 420, 420);
+#define motor_shaft_height 16
+#define color_distance 8
+*/
+
+//golden_1_rotated_2017 02 28 10 17 00
+/*
+string input_file_name = "c:\\Mihai\\Dropbox\\fruits\\golden_1_rotated_2017 02 28 10 17 00";
+Rect r_box(778, 200, 405, 405);
+#define motor_shaft_height 5
+#define color_distance 8
+*/
+
+//golden_2_2017 02 28 10 23 44
+/*
+string input_file_name = "c:\\Mihai\\Dropbox\\fruits\\golden_2_2017 02 28 10 23 44";
+Rect r_box(770, 255, 455, 455);
+#define motor_shaft_height 1
+#define color_distance 5
+*/
+
+//golden_2_rotated_2017 02 28 10 24 36
+/*
+string input_file_name = "c:\\Mihai\\Dropbox\\fruits\\golden_2_rotated_2017 02 28 10 24 36";
+Rect r_box(730, 200, 515, 515);
+#define motor_shaft_height 17
+#define color_distance 6
+*/
+
+//golden_3_2017 03 05 08 31 35
+/*
+string input_file_name = "c:\\Mihai\\Dropbox\\fruits\\golden_3_2017 03 05 08 31 35";
+Rect r_box(1000, 400, 500, 500);
+#define motor_shaft_height 19
+#define color_distance 6
+*/
+
+//golden_3_rotated_2017 03 05 08 32 02
+string input_file_name = "c:\\Mihai\\Dropbox\\fruits\\golden_3_rotated_2017 03 05 08 32 02";
+Rect r_box(980, 250, 550, 550);
+#define motor_shaft_height 8
+#define color_distance 5
