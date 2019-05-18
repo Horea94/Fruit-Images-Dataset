f6a51aeb4e8c904fcd3e6b7e1728a0a843d2e9ef
horea94 <horea94@gmail.com>
2019-05-18 12:56:06 +0300

    Determine the image count dinamically

--- src/image_classification/utils/constants.py
+++ src/image_classification/utils/constants.py
@@ -11,8 +11,12 @@ test_images_dir = 'D:\\Robots\\Fruit-Images-Dataset\\Test'
 
 # number of classes: number of fruit classes + 1 resulted due to the build_image_data.py script that leaves the first class as a background class
 # using the labels file that is also used in the build_image_data.py
+
+number_train_images = 0
+number_test_images = 0
 with open(labels_file) as f:
-    labels = f.readlines()
+    labels = [line.rstrip() for line in f]
     num_classes = len(labels) + 1
-number_train_images = 53177
-number_test_images = 17845
+    for label in labels:
+        number_train_images += len(os.listdir(os.path.join(training_images_dir, label)))
+        number_test_images += len(os.listdir(os.path.join(test_images_dir, label)))
